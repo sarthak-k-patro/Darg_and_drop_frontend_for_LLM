431f56d2ff4014288cf2cbd8f542a85bf8e2bd8d
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2025-01-04 19:22:42 +0530

    changes for adding variable in textNode and also style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/screens/**/*.{js,jsx,ts,tsx}",
+    "./src/styles/**/*.{css,module.css}",
+  ],
   darkMode: "class",
   theme: {
     extend: {},
